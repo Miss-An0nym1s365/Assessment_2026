200be7cf9420f0f8f59f67a8d5f1af0cec90d656
Miss-An0nym1s365 <jimmyc24942@masseyhigh.school.nz>
2026-05-15 09:53:50 +1200

    generatior/answer WIP

--- C_03_number_generator.py
+++ C_03_number_generator.py
@@ -1,1 +1,16 @@
- def equation_gen = input(question("what math type?")):
+from B_01_rps_game_v2 import feedback
+
+
+def answer_check(question):
+     """checks if user got question right, with answer checked by computer"""
+
+question = 7+3
+
+answer = answer_check(f"What is {question} ? ")
+
+if user input: "10"
+    feedback"you got it right "
+else:
+    feedback = "u got it wrong..."
+
+    
