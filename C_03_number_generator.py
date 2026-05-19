22685f2ea221b705b26cf83c8968fe5669de48bd
Miss-An0nym1s365 <jimmyc24942@masseyhigh.school.nz>
2026-05-19 16:14:36 +1200

    Testing WIP

--- C_03_number_generator.py
+++ C_03_number_generator.py
@@ -1,22 +1,24 @@
 import random
 
-def user_response(question):
-    """generates numbers for equations and stuff"""
-    print(question)
-
-    error = "Please enter a valid integer answer..."
-
-    question = f"What is {num_choose} + {num_choose} = "
+def math_ans(question):
+    """users see if their answers are right"""
 
 num_choose = random.randint(1, 1000)
 
+user_response = []
+
 check_ans = num_choose + num_choose
 
-if user_response == check_ans:
-    print("HOORAY! you got it correct :)")
-elif user_response != check_ans:
-    print(f"The answer was actually {check_ans}")
-elif user_response == "xxx":
-    print("You chose to exit the program")
-else:
-    print(error)
+while True:
+    if user_response == check_ans:
+        print("HOORAY! you got it correct :)")
+    elif user_response != check_ans:
+        print(f"The answer was actually {check_ans}")
+    elif user_response == "xxx":
+        print("You chose to exit the program")
+    else:
+        error = "Please enter a valid integer answer..."
+
+# main routine
+
+give_ans = math_ans(f"What is {num_choose} + {num_choose} = ")
