bafaffd769cba4cb764fde24a583553a963da378
Miss-An0nym1s365 <jimmyc24942@masseyhigh.school.nz>
2026-05-21 15:06:40 +1200

    just genreator. almost success

--- C_03_equation_generator.py
+++ C_03_equation_generator.py
@@ -1,33 +1,38 @@
 import random
 
-def equation(question):
+def equation_gen(question):
     """generates equations"""
 
-error = "Please enter a valid inter answer..."
+# error = "Please enter a valid inter answer..."
 
 num_choose = random.randint(1, 1000)
 num_choose2 = random.randint(1, 1000)
 
 math_expression = f"{num_choose} + {num_choose2} "
 
-question = math_expression
+for item in range(0, 5):
+    equation_gen = random.choice()
 
-# print(math_expression)
+print(math_expression)
 
-math_answer = eval(question)
 
-# print(f"The answer is {math_answer}")
-
-while True:
-
-    user_response = input(question)
-
-    if user_response == math_answer:
-        print("HOORAY! You got it correct 🙌")
-    elif user_response != math_answer:
-        print(f"Sorry, the answer was actually {math_answer}")
-    elif user_response == "xxx":
-        print("You chose to exit the program")
-    else:
-        print(error)
-# main routine
+#
+# math_answer = eval(math_expression)
+#
+# # print(f"The answer is {math_answer}")
+#
+# while True:
+#
+#     user_response = input(math_expression)
+#
+#     if user_response == math_answer:
+#         print("HOORAY! You got it correct 🙌")
+#     elif user_response == "xxx":
+#         print("You chose to exit the program")
+#     elif user_response != math_answer:
+#         print(f"Sorry, the answer was actually {math_answer}")
+#     else:
+#         print(error)
+# # main routine
+#
+#     print("end of question")
