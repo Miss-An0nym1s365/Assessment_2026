5c125b21b36ac6b099941f0f21ee44d101a66bf1
Miss-An0nym1s365 <jimmyc24942@masseyhigh.school.nz>
2026-05-25 20:01:13 +1200

    working answer checker

--- C_05_Ans_Checker.py
+++ C_05_Ans_Checker.py
@@ -1,4 +1,32 @@
-def ans_check(question):
+import random
+def answer_check(user_answer, correct_answer):
+    """Checks if the user's answer is correct"""
 
-    question = "DEAR GOD HELP ME"
-    print(question)
+    if user_answer == correct_answer:
+        print("Correct!")
+    else:
+        print("Incorrect!")
+        print("The correct answer was", correct_answer)
+
+while True:
+
+    num_choose = random.randint(1, 1000)
+    num_choose2 = random.randint(1, 1000)
+
+    math_expression = f"{num_choose} + {num_choose2}"
+
+    print()
+    print(math_expression)
+
+    correct_answer = num_choose + num_choose2
+
+    user_answer = int(input("Your answer: "))
+
+    answer_check(user_answer, correct_answer)
+
+    again = input("Press <enter> to continue or 'xxx' to quit: ")
+
+    if again == "xxx":
+        print()
+        print("Program continues...")
+        break
