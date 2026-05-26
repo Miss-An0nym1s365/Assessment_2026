94930001ed8bdb6b1f966666345a176e713a05e6
Miss-An0nym1s365 <jimmyc24942@masseyhigh.school.nz>
2026-05-26 16:16:00 +1200

    WORK

--- C_04_Num_check.py
+++ C_04_Num_check.py
@@ -1,4 +1,4 @@
-def num_check(question, num_type=int, low=0,high=2000, exit_code="xxx"):
+def num_check(question, num_type=int, low=-1,high=2001, exit_code="xxx"):
     """checks the user responses are valid"""
 
     while True:
@@ -13,9 +13,9 @@ def num_check(question, num_type=int, low=0,high=2000, exit_code="xxx"):
             response = num_type(response)
 
             if response <= low:
-                print(f"Error: enter a number higher than {low}.")
+                print(f"Error: enter a number higher than (or equal to) 0.")
             elif response >= high:
-                print(f"Error: Please enter a number lower than {high}")
+                print(f"Error: Please enter a number lower than (or equal to) 2000")
             else:
                 return response
 
--- C_05_Ans_Checker.py
+++ C_05_Ans_Checker.py
@@ -1,4 +1,31 @@
-def ans_check(question):
+import random
+def ans_check(user_answer, correct_answer):
+    """Checks user's answer is correct"""
 
-    question = "DEAR GOD HELP ME"
-    print(question)
+    if user_answer == correct_answer:
+        print("Correct-a-mundo! You got it right!")
+    else:
+        print("Oof, Incorrect...")
+        print("The correct answer was ", correct_answer)
+
+
+while True:
+
+    num_choose = random.randint(1, 1000)
+    num_choose2 = random.randint(1, 1000)
+
+    math_expression = f"{num_choose} + {num_choose2}"
+
+    print()
+    print(math_expression)
+
+    correct_answer = num_choose + num_choose2
+
+    user_answer =int(input("Your answer: "))
+
+    ans_check(user_answer,correct_answer)
+    again = input("Press <enter> to continue or 'xxx' to quit: ")
+    if again == "xxx":
+        print()
+        print("Program continues...")
+        break
